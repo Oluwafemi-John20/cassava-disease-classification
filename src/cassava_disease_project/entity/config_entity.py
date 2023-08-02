b55311783858fcd9617e504b77bdaff7be00e7d0
Oluwafemi-John20 <adeyemofemi19@gmail.com>
2023-08-02 19:21:13 +0100

    Prepare base model completed

--- src/cassava_disease_project/entity/config_entity.py
+++ src/cassava_disease_project/entity/config_entity.py
@@ -9,3 +9,14 @@ class DataIngestionConfig:
     unzip_dir: Path
     train_dir: Path
     test_dir: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    update_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
