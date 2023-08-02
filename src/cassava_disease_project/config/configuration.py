f495f30518569f96dc24d0e3d1c9dae8d78beaeb
Oluwafemi-John20 <adeyemofemi19@gmail.com>
2023-08-02 22:00:07 +0100

    Prepare callbacks added

--- src/cassava_disease_project/config/configuration.py
+++ src/cassava_disease_project/config/configuration.py
@@ -1,7 +1,8 @@
 from cassava_disease_project.constants import *
 from cassava_disease_project.utils.common import read_yaml, create_directories
 from cassava_disease_project.entity.config_entity import (DataIngestionConfig, 
-                                                          PrepareBaseModelConfig)
+                                                          PrepareBaseModelConfig,
+                                                          PrepareCallbacksConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -47,4 +48,19 @@ class ConfigurationManager:
             params_classes= self.params.CLASSES
         )
         return prepare_base_model_config
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callbacks_config = PrepareCallbacksConfig(
+            root_dir = Path(config.root_dir),
+            tensorboard_root_log_dir= Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath= Path(config.checkpoint_model_filepath)
+        )
+        
+        return prepare_callbacks_config
     
