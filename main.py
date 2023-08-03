436154db18ae6ab4862fb629e70528bd78911a4e
Oluwafemi-John20 <adeyemofemi19@gmail.com>
2023-08-03 10:05:56 +0100

    Callbacks and Training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cassava_disease_project import logger
 from cassava_disease_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cassava_disease_project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cassava_disease_project.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -23,3 +24,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Training"
+if __name__ == "__main__":
+    try:
+        logger.info(f"********************************")
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<\n\nx============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
